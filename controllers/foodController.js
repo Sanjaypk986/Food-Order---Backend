816626e4b3c4b821bd27d4777af6472c7a6a5900
SANJAY <sanjaypk986@gmail.com>
2024-08-15 19:31:18 +0530

    delete food controller implemented

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -112,3 +112,24 @@ export const getAllFoods = async (req, res) => {
       .json({ message: error.message || "Internal server error" });
   }
 };
+
+// delete food
+export const deleteFood = async (req, res) => {
+    try {
+        // get food id 
+        const {foodId} = req.params
+      // find foods
+      const deletedFood = await Food.findByIdAndDelete(foodId);
+      if (!deletedFood) {
+        return res.status(404).json({ success: false, message: 'Food item not found' });
+      }
+      res
+        .status(200)
+        .json({ success: true, message: "food deleted successfully",});
+    } catch (error) {
+      // send error response
+      res
+        .status(error.status || 500)
+        .json({ message: error.message || "Internal server error" });
+    }
+  };
--- routes/v1/foodRoutes.js
+++ routes/v1/foodRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { foodCreate, foodUpdate, getAllFoods, getFoodById } from '../../controllers/foodController.js'
+import { deleteFood, foodCreate, foodUpdate, getAllFoods, getFoodById } from '../../controllers/foodController.js'
 import { upload } from '../../middlewares/uploadMiddleware.js'
 
 const router = express.Router()
@@ -8,6 +8,7 @@ router.get('/foods',getAllFoods)
 router.get('/foods/:foodId',getFoodById)
 router.post('/create',upload.single('foodImage'),foodCreate)
 router.patch('/update/:foodId',upload.single('foodImage'),foodUpdate)
+router.delete('/delete/:foodId',deleteFood)
 
 
 export default router
