23650dc270313ec425296ebbab3ad46dc3fb9aed
SANJAY <sanjaypk986@gmail.com>
2024-09-03 15:45:37 +0530

    trying to fixx errors in logout

--- controllers/userController.js
+++ controllers/userController.js
@@ -136,19 +136,15 @@ export const loginUser = async (req, res) => {
 };
 
 export const logoutUser = async (req, res) => {
-  try {
-    // Clear the token by setting it to an empty string and expiring immediately
-    // res.cookie("token", "", {
-    //   expires: new Date(0),
-    //   httpOnly: true,
-    //   secure: true, 
-    //   sameSite: "none", 
-    // });
-    console.log(res.cookie);
-    res.clearCookie("token")
-    console.log(res.cookie);
-    
-    console.log('user logout');
+  
+    try {
+      // delete the cookie 
+      res.cookie("token", "", {
+        expires: new Date(0),
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+      });
 
     res.status(200).json({ success: true, message: "User logout successfully" });
   } catch (error) {
